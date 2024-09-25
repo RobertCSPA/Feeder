c5b980709460553a2213827fac147ae4b71fb8bd
RobertCSPA <robertf127@nycstudents.net>
2024-09-25 16:19:56 +0000

    add feeder

--- Main.java
+++ Main.java
@@ -6,8 +6,22 @@ public class Main
         System.out.println(f);
         System.out.println(f.getCurrentFood());
         System.out.println(Math.random());
-        int random = (int)(Math.random() * 10);
+        int random = (int)(Math.random() * 10 + 1);
         System.out.println(random);
+        System.out.println(Math.random() < 0.05);
+        Feeder g = new Feeder(500);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+        
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
+
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
     }
 
 }
